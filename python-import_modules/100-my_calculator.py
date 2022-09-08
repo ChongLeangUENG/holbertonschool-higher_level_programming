600ff8d3e0b21ca11468f891fa24daec19a40071
ChongLeangUENG <leangcnrp777@gmail.com>
2022-09-08 05:43:19 -0700

    Mu calculator in python

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -18,10 +18,9 @@ if __name__ == '__main__':
 
         if argv[2] == "+":
             print("{} + {} = {}".format(a, b, add(a, b)))
-        if argv[2]== "-":
+        if argv[2] == "-":
             print("{} - {} = {}".format(a, b, sub(a, b)))
         if argv[2] == "*":
             print("{} * {} = {}".format(a, b, mul(a, b)))
         if argv[2] == "/":
             print("{} / {} = {}".format(a, b, div(a, b)))
-
