4fd6ea95abc5153d7b7c47e2dab065e91d368b06
ChongLeangUENG <leangcnrp777@gmail.com>
2022-09-13 00:16:13 -0700

    Print matrix of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
+        #range i for print vertical line
         for j in range(len(matrix[i])):
+            #range j for print horizontal line
             if j != 0:
                 print(" ", end="")
             print("{:d}".format(matrix[i][j]), end="")
