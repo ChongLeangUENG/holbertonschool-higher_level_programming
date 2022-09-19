afefb5c1b3205e54749de24629196999ac6c747b
ChongLeangUENG <leangcnrp777@gmail.com>
2022-09-19 06:44:12 -0700

    class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """a square"""
+    def __init__(self, size):
+        """function that create a square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
