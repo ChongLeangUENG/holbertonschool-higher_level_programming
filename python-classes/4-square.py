1344360cc9eb4368fd224215ef216f37f8303cd6
ChongLeangUENG <leangcnrp777@gmail.com>
2022-09-20 06:47:35 -0700

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -25,3 +25,30 @@ class Square:
         int: the current square area
         """
         return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """getting the size
+
+        Returns:
+            int: size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setting the size
+
+        Args:
+            value (int): the new size
+
+        Raises:
+            TypeError: exception
+            ValueError: exception
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
