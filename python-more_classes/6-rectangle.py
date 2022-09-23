74c049a851c71ae5f71a6a99eb90828af02bf771
ChongLeangUENG <leangcnrp777@gmail.com>
2022-09-23 02:22:39 -0700

    Function that count instance in class rectangle

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,6 +7,7 @@ Define a rectangle class
 class Rectangle:
     """defines a rectangle"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialize a new rectangle
 
