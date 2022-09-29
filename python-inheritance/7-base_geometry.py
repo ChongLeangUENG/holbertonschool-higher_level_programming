770338b89fcaa8ff70d7584f0f0ccca77bd93e8b
ChongLeangUENG <leangcnrp777@gmail.com>
2022-09-29 01:29:04 -0700

    Correction of test cases

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,7 +14,7 @@ class BaseGeometry():
             name: a string
             value: Value of geometry
         """
-        if isinstance(value, int) is False:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
