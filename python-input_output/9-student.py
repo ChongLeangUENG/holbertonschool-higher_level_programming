d18b0827f2edbe1dbe7bc89f06b19ddf1f572274
ChongLeangUENG <leangcnrp777@gmail.com>
2022-09-29 14:43:38 +0200

    Create a student in class

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,11 +4,13 @@
 
 class Student:
     """The Student Class"""
+
     def __init__(self, first_name, last_name, age):
         """Create a student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """Class student to JSON"""
         return self.__dict__
