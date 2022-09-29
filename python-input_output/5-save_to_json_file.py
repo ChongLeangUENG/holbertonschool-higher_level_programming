2ac8f1119cf8c8df720325f9191999e6a3bac6bd
ChongLeangUENG <leangcnrp777@gmail.com>
2022-09-29 11:27:00 +0200

    Save a text file to json;

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,7 +7,7 @@ def save_to_json_file(my_obj, filename):
     """save object in json
         Args:
             my_obj: Object
-            filename: String file   
+            filename: String file
     """
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(my_obj, file)
