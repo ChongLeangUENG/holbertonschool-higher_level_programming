3f372d959af5391778998180bc45cf3d1a92d480
ChongLeangUENG <leangcnrp777@gmail.com>
2022-11-04 20:54:40 +0100

    Temperatures #0

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Import in hbtn_0c_0 database the table dump
-SELECT city, AVG(value) AS avg_tmp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY avg_tmp DESC
+ORDER BY avg_temp DESC
