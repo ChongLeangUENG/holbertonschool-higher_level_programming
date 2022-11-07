d44e180a2615f4f64e98e6d51c374b193d3fcef3
ChongLeangUENG <leangcnrp777@gmail.com>
2022-11-07 15:38:08 +0100

    Cities of California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- lists all the cities of California that can be found in the database
+SELECT id, name -- Query to list all the cities from California
+FROM cities
+WHERE state_id = ( -- Query to get the id of California
+    SELECT id
+    FROM states
+    WHERE name = "California"
+);
