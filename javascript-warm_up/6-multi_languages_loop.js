ad58c77b6227b37d9675b17bad854992fcfffba2
ChongLeangUENG <leangcnrp777@gmail.com>
2022-12-08 14:52:55 +0100

    Loop to multi languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let index = 0;
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 while (index in arr) {
   console.log(arr[index]);
   index++;
