4b3c41d5678fd363e0f2ae458dbf68269cc9c6a8
ChongLeangUENG <leangcnrp777@gmail.com>
2022-12-12 13:51:46 +0100

    Status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/local/bin/node
+const request = require('request');
+request.get(process.argv[2], function (error, response, body) {
+  if (!error) {
+    console.log('code: ', response.statusCode);
+  }
+});
