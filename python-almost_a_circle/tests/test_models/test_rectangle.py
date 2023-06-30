d649cc81c3697a22efe5f7be454e58f9ff0001a4
ChongLeang UENG <105638150+ChongLeangUENG@users.noreply.github.com>
2023-06-30 10:50:31 +0200

    Update test_rectangle.py

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,110 +1,126 @@
 #!/usr/bin/python3
-
+"Unit tests for Rectangle class"
 import unittest
+from unittest import mock
+import io
+import os
 from models.rectangle import Rectangle
-from models.base import Base
+
 
 class TestRectangle(unittest.TestCase):
+    """Unit test suite for Rectangle class"""
 
     def test_init(self):
-        # test case 1: rectangle with width 10 and height 5
-        r1 = Rectangle(10, 5)
-        self.assertEqual(r1.width, 10)
-        self.assertEqual(r1.height, 5)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
-        self.assertIsInstance(r1, Rectangle)
-        self.assertIsInstance(r1, Base)
+        """Test of Rectangle for width/height and default initialization"""
+        r = Rectangle(1, 2)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
+        r = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r.id, 5)
 
-    def test_init_with_id(self):
-        # test case 1: rectangle with width 7, height 3, x 2, y 4, and id 1
-        r2 = Rectangle(7, 3, 2, 4, 1)
-        self.assertEqual(r2.width, 7)
-        self.assertEqual(r2.height, 3)
-        self.assertEqual(r2.x, 2)
-        self.assertEqual(r2.y, 4)
-        self.assertEqual(r2.id, 1)
-        self.assertIsInstance(r2, Rectangle)
-        self.assertIsInstance(r2, Base)
+    def test_type(self):
+        """Test of Rectangle for wrong attribute types"""
+        self.assertRaisesRegex(
+            TypeError, "width must be an integer", Rectangle, "1", 2)
+        self.assertRaisesRegex(
+            TypeError, "height must be an integer", Rectangle, 1, "2")
+        self.assertRaisesRegex(
+            TypeError, "x must be an integer", Rectangle, 1, 2, "3")
+        self.assertRaisesRegex(
+            TypeError, "y must be an integer", Rectangle, 1, 2, 3, "4")
 
-    def test_width(self):
-        # test case 1: set width to 8
-        r3 = Rectangle(10, 5)
-        r3.width = 8
-        self.assertEqual(r3.width, 8)
+    def test_value(self):
+        """Test of Rectangle for invalid values"""
+        self.assertRaisesRegex(
+            ValueError, "width must be > 0", Rectangle, -1, 2)
+        self.assertRaisesRegex(
+            ValueError, "height must be > 0", Rectangle, 1, -2)
+        self.assertRaisesRegex(
+            ValueError, "width must be > 0", Rectangle, 0, 2)
+        self.assertRaisesRegex(
+            ValueError, "height must be > 0", Rectangle, 1, 0)
+        self.assertRaisesRegex(
+            ValueError, "x must be >= 0", Rectangle, 1, 2, -3)
+        self.assertRaisesRegex(
+            ValueError, "y must be >= 0", Rectangle, 1, 2, 3, -4)
 
-    def test_width_with_value_error(self):
-        # test case 1: set width to -8, expect ValueError
-        with self.assertRaises(ValueError):
-            r4 = Rectangle(10, 5)
-            r4.width = -8
+    def test_area(self):
+        """Tests if Rectangle's area() exists and returns the right value"""
+        r = Rectangle(3, 2)
+        self.assertEqual(r.area(), 6)
 
-    def test_width_with_type_error(self):
-        # test case 1: set width to "8", expect TypeError
-        with self.assertRaises(TypeError):
-            r5 = Rectangle(10, 5)
-            r5.width = "8"
+    def test_display(self):
+        """Tests if Rectangle's display() exists and prints right value"""
+        # Test for no y
+        r = Rectangle(3, 2, 1, 0)
+        with mock.patch("sys.stdout", new=io.StringIO()) as mocked_stdout:
+            r.display()
 
-    def test_height(self):
-        # test case 1: set height to 3
-        r6 = Rectangle(10, 5)
-        r6.height = 3
-        self.assertEqual(r6.height, 3)
+        assert mocked_stdout.getvalue() == " ###\n ###\n"
 
-    def test_height_with_value_error(self):
-        # test case 1: set height to -3, expect ValueError
-        with self.assertRaises(ValueError):
-            r7 = Rectangle(10, 5)
-            r7.height = -3
+        # Test for neither x nor y
+        r = Rectangle(3, 2, 0, 0)
+        with mock.patch("sys.stdout", new=io.StringIO()) as mocked_stdout:
+            r.display()
 
-    def test_height_with_type_error(self):
-        # test case 1: set height to "3", expect TypeError
-        with self.assertRaises(TypeError):
-            r8 = Rectangle(10, 5)
-            r8.height = "3"
+        assert mocked_stdout.getvalue() == "###\n###\n"
 
-    def test_x(self):
-        # test case 1: set x to 2
-        r9 = Rectangle(10, 5)
-        r9.x = 2
-        self.assertEqual(r9.x, 2)
+    def test_str(self):
+        """Tests if Rectangle's str representation
+        exists and has right format"""
+        r = Rectangle(4, 6, 2, 1, 12)
+        self.assertEqual(str(r), "[Rectangle] (12) 2/1 - 4/6")
 
-    def test_x_with_value_error(self):
-        # test case 1: set x to -2, expect ValueError
-        with self.assertRaises(ValueError):
-            r10 = Rectangle(10, 5)
-            r10.x = -2
+    def test_update(self):
+        """Tests if Rectangle's update() exists and updates the right args"""
+        r = Rectangle(10, 20, 30, 40, 50)
+        r.update(89, 1, 2, 3, 4)
+        self.assertEqual(r.id, 89)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 3)
+        self.assertEqual(r.y, 4)
 
-    def test_x_with_type_error(self):
-        # test case 1: set x to "2", expect TypeError
-        with self.assertRaises(TypeError):
-            r11 = Rectangle(10, 5)
-            r11.x = "2"
+    def test_create(self):
+        """tests create"""
+        to_test = Rectangle.create(**{'id': 89, 'width': 1,
+                                      'height': 2, 'x': 3})
+        answer = Rectangle(1, 2, 3, 0, 89)
+        self.assertEqual(str(to_test), str(answer))
 
-    def test_y(self):
-        # test case 1: set y to 4
-        r12 = Rectangle(10, 5)
-        r12.y = 4
-        self.assertEqual(r12.y, 4)
+        to_test = Rectangle.create(**{'id': 89, 'width': 1,
+                                      'height': 2, 'x': 3, 'y': 4})
+        answer = Rectangle(1, 2, 3, 4, 89)
+        self.assertEqual(str(to_test), str(answer))
 
-    def test_y_with_value_error(self):
-        # test case 1: set y to -4, expect ValueError
-        with self.assertRaises(ValueError):
-            r13 = Rectangle(10, 5)
-            r13.y = -4
+        to_test = Rectangle.create(
+            **{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        answer = Rectangle(1, 2, 3, 0, 89)
+        self.assertEqual(str(to_test), str(answer))
 
-    def test_additional_type_error(self):
-        # test case 1: set width to '', expect TypeError
-        with self.assertRaises(TypeError):
-            r14 = Rectangle('', 5)
-            raise TypeError()
+        to_test = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2})
+        answer = Rectangle(1, 2, 0, 0, 89)
+        self.assertEqual(str(to_test), str(answer))
 
-    def test_additional_type_error_2(self):
-        # test case 1: set x to 1.50, expect TypeError
-        with self.assertRaises(TypeError):
-            Rectangle(5, 1, 45.90)
-            raise TypeError()
+        to_test = Rectangle.create(**{'id': 89, 'width': 1})
+        self.assertEqual(to_test.id, 89)
+        self.assertEqual(to_test.width, 1)
+        self.assertEqual(to_test.x, 0)
+        self.assertEqual(to_test.y, 0)
+
+        to_test = Rectangle.create(**{'id': 89})
+        self.assertEqual(to_test.id, 89)
+        self.assertEqual(to_test.x, 0)
+        self.assertEqual(to_test.y, 0)
+
+    def test_dictionary(self):
+        s1 = Rectangle(10, 2, 1, 9)
+        s1_dict = s1.to_dictionary()
+        self.assertEqual(s1_dict, {'x': 1, 'y': 9, 'id': 9,
+                                   'height': 2, 'width': 10})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
